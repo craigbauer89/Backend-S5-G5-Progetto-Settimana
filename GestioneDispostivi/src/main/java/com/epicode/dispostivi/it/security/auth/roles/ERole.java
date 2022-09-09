868fde389ae22537dbdfee586714ed8346b6f483
craigbauer89 <craigbauer89@gmail.com>
2022-09-09 15:31:08 +0200

    Organisazione dei pachetti e sicurezza

---
+++ GestioneDispostivi/src/main/java/com/epicode/dispostivi/it/security/auth/roles/ERole.java
@@ -0,0 +1,6 @@
+package com.epicode.dispostivi.it.security.auth.roles;
+
+public enum ERole {
+	ROLE_ADMIN,
+	ROLE_USER;
+}
---
+++ GestioneDispostivi/src/main/java/com/epicode/dispostivi/it/security/auth/roles/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.epicode.dispostivi.it.security.auth.roles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+
+
+}
