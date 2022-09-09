e0af03007699af80ee5f27b1446c5e8b1c0fdd49
craigbauer89 <craigbauer89@gmail.com>
2022-09-09 12:09:32 +0200

    Primo commit

---
+++ GestioneDispostivi/src/main/java/com/epicode/dispostivi/GestioneDispostiviApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.dispostivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneDispostiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneDispostiviApplication.class, args);
+	}
+
+}
---
+++ GestioneDispostivi/src/test/java/com/epicode/dispostivi/GestioneDispostiviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.dispostivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispostiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
